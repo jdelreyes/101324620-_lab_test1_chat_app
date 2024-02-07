55491e7238b2f61e0da38f5ec157affa6bac0350
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-02-07 14:19:35 -0500

    feat(schema): hash user password at model level

--- server/model/User.ts
+++ server/model/User.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from 'mongoose';
 import { IUser } from '../interface/IUser';
+import * as argon from 'argon2';
 
 const userSchema: Schema<IUser> = new Schema({
   userName: {
@@ -24,5 +25,38 @@ const userSchema: Schema<IUser> = new Schema({
   },
   createdAt: {
     type: Date,
+  },
+});
+
+userSchema.pre('save', async function (this: IUser, next): Promise<void> {
+  if (!this.isModified('password')) return next();
+
+  try {
+    this.password = await argon.hash(this.password.toString());
+    next();
+  } catch (error) {
+    console.error(`[ERROR] ${error}`);
+    return next(error);
   }
 });
+
+userSchema.pre(
+  'findOneAndUpdate',
+  async function (this: { _update: IUser }, next): Promise<void> {
+    if (!this._update || !this._update.password) return next();
+
+    try {
+      this._update.password = await argon.hash(
+        this._update.password.toString(),
+      );
+      next();
+    } catch (error) {
+      console.error(`[ERROR] ${error}`);
+      return next(error);
+    }
+  },
+);
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
